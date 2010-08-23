4eb65cfcb9a23ddbd5683751de8c4ea7f6c3d717
MdeJ_cp <SND\MdeJ_cp>
2010-08-23 10:35:32 -0700

    Added source code for EntityGraph
    The entity graph concept enables defining graphs of related entities which form a unit. Entity graph methods operate an entity graph as a whole. For example, detaching an entity from a context, will detach all entities in that graph from the context. Cloning an entity will clone all entities in the graph, and deleting an entity will delete all entities in the graph (similar to 'cascading deletes' in data bases).

---
+++ RiaServicesContrib/EntityGraph/EntityGraphAttribute.shared.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace System.ComponentModel.DataAnnotations
+{
+    /// <summary>
+    /// Specifies that an association is part of a graph of related entities 
+    /// that should be handled as a unit during cloning, detaching, deleting, and so on.
+    /// </summary>
+    public class EntityGraphAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets/sets the name of the entity graph
+        /// </summary>
+        public string Name { get; private set; }
+        /// <summary>
+        /// Constructor. Sets the name of the entity graph to 'name'
+        /// </summary>
+        /// <param name="name"></param>
+        public EntityGraphAttribute(string name)
+        {
+            this.Name = name;
+        }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public EntityGraphAttribute() { Name = null;  }
+    }
+}
