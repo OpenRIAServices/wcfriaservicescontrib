fed0d1f1b98beb699f180152c2c1a19e85b30e47
MdeJ_cp <SND\MdeJ_cp>
2010-11-05 13:32:06 -0700

    Proxy class uses Singleton pattern for creating entity graphs.
    This means that there cal always be a single entity graph instance for
    a particular graphtype and entity type. This is experimental, but does make
    sense, because e.g., calling a.EntityGraph() twice should return the same
    object twice, and not create two different EntityGRaph instances.

--- RiaServicesContrib/EntityGraph/EntityGraph/RIA/Proxies.cs
+++ RiaServicesContrib/EntityGraph/EntityGraph/RIA/Proxies.cs
@@ -1,4 +1,6 @@
 ﻿using System.ServiceModel.DomainServices.Client;
+using System;
+using System.Collections.Generic;
 
 namespace EntityGraph.RIA
 {
@@ -12,7 +14,8 @@ namespace EntityGraph.RIA
         /// <returns></returns>
         public static EntityGraph<TEntity> EntityGraph<TEntity>(this TEntity entity) where TEntity : Entity
         {
-            return new EntityGraph<TEntity>(entity);
+            return GraphFactory<TEntity>.GetAttributedEntityGraph(entity, null);
+//            return new EntityGraph<TEntity>(entity);
         }
         /// <summary>
         /// Extension method that returns an entity graph object that represents the entity graph with given name this entity is part of.
@@ -23,7 +26,8 @@ namespace EntityGraph.RIA
         /// <returns></returns>
         public static EntityGraph<TEntity> EntityGraph<TEntity>(this TEntity entity, string graphName) where TEntity : Entity
         {
-            return new EntityGraph<TEntity>(entity, graphName);
+            return GraphFactory<TEntity>.GetAttributedEntityGraph(entity, graphName);
+//            return new EntityGraph<TEntity>(entity, graphName);
         }
         /// <summary>
         /// Extension method that returns an entity graph object, defined by the provided collection of paths
@@ -34,11 +38,13 @@ namespace EntityGraph.RIA
         /// <returns></returns>
         public static EntityGraph<TEntity> EntityGraph<TEntity>(this TEntity entity, string[] paths) where TEntity : Entity
         {
-            return new EntityGraph<TEntity>(entity, paths);
+            return GraphFactory<TEntity>.GetStringPathsEntityGraph(entity, paths);
+//            return new EntityGraph<TEntity>(entity, paths);
         }
         public static EntityGraph<TEntity> EntityGraph<TEntity>(this TEntity entity, EntityGraphShape<TEntity, Entity> shape) where TEntity : Entity
         {
-            return new EntityGraph<TEntity>(entity, shape);
+            return GraphFactory<TEntity>.GetGraphShapeEntityGraph(entity, shape);
+            //            return new EntityGraph<TEntity>(entity, shape);
         }
 
         /// <summary>
@@ -79,5 +85,50 @@ namespace EntityGraph.RIA
         {
             return new EntityGraph<TEntity>(entity, shape).Clone();
         }
+        #region Factory
+        private static class GraphFactory<TEntity> where TEntity : Entity
+        {
+            private static Dictionary<Tuple<int, string>, EntityGraph<TEntity>> AttributedEntityGraphs
+                = new Dictionary<Tuple<int, string>, EntityGraph<TEntity>>();
+            private static Dictionary<Tuple<int, string[]>, EntityGraph<TEntity>> StringPathsEntityGraphs
+                = new Dictionary<Tuple<int, string[]>, EntityGraph<TEntity>>();
+            private static Dictionary<Tuple<int, EntityGraphShape<TEntity, Entity>>, EntityGraph<TEntity>> GraphShapeEntityGraphs
+                = new Dictionary<Tuple<int, EntityGraphShape<TEntity, Entity>>, EntityGraph<TEntity>>();
+
+            public static EntityGraph<TEntity> GetAttributedEntityGraph(TEntity entity, string graphName)
+            {
+                var key = new Tuple<int, string>(entity.GetHashCode(), graphName);
+                if(AttributedEntityGraphs.ContainsKey(key) == false)
+                {
+                    var gr = new EntityGraph<TEntity>(entity, graphName);
+                    AttributedEntityGraphs.Add(key, gr);
+                    return gr;
+                }
+                return AttributedEntityGraphs[key];
+            }
+            public static EntityGraph<TEntity> GetStringPathsEntityGraph(TEntity entity, string[] paths)
+            {
+                var key = new Tuple<int, string[]>(entity.GetHashCode(), paths);
+                if(StringPathsEntityGraphs.ContainsKey(key) == false)
+                {
+                    var gr = new EntityGraph<TEntity>(entity, paths);
+                    StringPathsEntityGraphs.Add(key, gr);
+                    return gr;
+                }
+                return StringPathsEntityGraphs[key];
+            }
+            public static EntityGraph<TEntity> GetGraphShapeEntityGraph(TEntity entity, EntityGraphShape<TEntity, Entity> shape)
+            {
+                var key = new Tuple<int, EntityGraphShape<TEntity, Entity>>(entity.GetHashCode(), shape);
+                if(GraphShapeEntityGraphs.ContainsKey(key) == false)
+                {
+                    var gr = new EntityGraph<TEntity>(entity, shape);
+                    GraphShapeEntityGraphs.Add(key, gr);
+                    return gr;
+                }
+                return GraphShapeEntityGraphs[key];
+            }
+        }
+        #endregion
     }
 }
