f91ab924fb9bf9f070dedfd65667a8b1747c7df2
MdeJ_cp <SND\MdeJ_cp>
2011-05-18 05:33:31 -0700

    EntityGraph: Removed obsolete stufff

--- RiaServicesContrib/EntityGraph/EntityGraph/EntityValidator/RIA/ValidationRule.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-using RIA.EntityValidator;
-
-namespace EntityGraph.EntityValidator.RIA
-{
-    public abstract class ValidationRule<TRoot> : ValidationRule<TRoot, ValidationResult>
-    {
-    }
-}
