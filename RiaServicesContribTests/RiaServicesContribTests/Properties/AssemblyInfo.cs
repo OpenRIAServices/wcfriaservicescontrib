708bc754388d10fc5055b752f564b175da70be24
MdeJ_cp <SND\MdeJ_cp>
2011-05-20 12:17:44 -0700

    Created a unit test framework for RiaServicesContrib
    This framework is basically an empty Silverlight test project with a customized initialization method in App.xaml.cs/Application_Startup. In this method the UnitTestSystem is initialized to load all assemblies in the XAP that contain test classes (classes which are decorated with the TestClassAttribute). This means that to add new tests to the RiaServicesContrib unit tests you only need to add a reference to the project/assembly containing your tests. 
    Initially I've added references to EntityTools, DataValidationFrameWork, and EntityGraph

---
+++ RiaServicesContribTests/RiaServicesContribTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿// Copyright © Philips 2011
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RiaServicesContribTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Philips")]
+[assembly: AssemblyProduct("RiaServicesContribTests")]
+[assembly: AssemblyCopyright("Copyright © Philips 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b064a0e-1b48-410e-97d5-d451ddcd47d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
