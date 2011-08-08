dd87e81e5c86ce405873245c2ed9f733f26504b1
ColinBlair_cp <SND\ColinBlair_cp>
2011-08-08 12:52:37 -0700

    Scaffolding: Changed to contrib version, generated code now matches official snippets.

---
+++ Scaffolding/Package/content/RiaContribEFDomainService/Dummy7D62A6D1.cs
@@ -0,0 +1,11 @@
+﻿//using System;
+
+//public class Dummy7D62A6D1
+//{
+//    public Dummy7D62A6D1()
+//    {
+//    }
+//}
+
+
+//This file is here to get around NuGet's restrictions on having a package without content rely on one that does.
