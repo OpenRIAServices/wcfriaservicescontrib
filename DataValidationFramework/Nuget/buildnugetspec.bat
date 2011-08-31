b624a0d0ec543400620e78db0221ef8d7b5e5afd
MdeJ_cp <SND\MdeJ_cp>
2011-08-31 03:07:28 -0700

    EntityGraph: NuGet packages are now build in RELEASE mode

--- DataValidationFramework/Nuget/buildnugetspec.bat
+++ DataValidationFramework/Nuget/buildnugetspec.bat
@@ -2,10 +2,10 @@ set PKG=DataValidationFramework
 mkdir lib\sl4
 mkdir lib\net40
 
-msbuild ..\%PKG%.Net\%PKG%.Net.csproj
-msbuild ..\%PKG%.Silverlight\%PKG%.Silverlight.csproj
+msbuild ..\%PKG%.Net\%PKG%.Net.csproj /p:Configuration=Release
+msbuild ..\%PKG%.Silverlight\%PKG%.Silverlight.csproj /p:Configuration=Release
 
-copy ..\%PKG%.Net\Bin\Debug\%PKG%.Net.dll lib\net40
-copy ..\%PKG%.Silverlight\Bin\Debug\%PKG%.Silverlight.dll lib\sl4
+copy ..\%PKG%.Net\Bin\Release\%PKG%.Net.dll lib\net40
+copy ..\%PKG%.Silverlight\Bin\Release\%PKG%.Silverlight.dll lib\sl4
 
-subwcrev . %PKG%.nuspec.src %PKG%.nuspec
+subwcrev .. %PKG%.nuspec.src %PKG%.nuspec
