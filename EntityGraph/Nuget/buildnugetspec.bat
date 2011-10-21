e46b61b8a9fe232711bb1ae23631aac15931a277
MdeJ_cp <SND\MdeJ_cp>
2011-10-21 14:58:28 -0700

    Added /p:Configuration=Release to msbuild invocations

--- EntityGraph/Nuget/buildnugetspec.bat
+++ EntityGraph/Nuget/buildnugetspec.bat
@@ -2,8 +2,8 @@ set PKG=EntityGraph
 mkdir lib\sl4
 mkdir lib\net40
 
-msbuild ..\%PKG%.Net\%PKG%.Net.csproj
-msbuild ..\%PKG%.Silverlight\%PKG%.Silverlight.csproj
+msbuild ..\%PKG%.Net\%PKG%.Net.csproj /p:Configuration=Release
+msbuild ..\%PKG%.Silverlight\%PKG%.Silverlight.csproj /p:Configuration=Release
 
 copy ..\%PKG%.Net\Bin\Release\%PKG%.Net.dll lib\net40
 copy ..\%PKG%.Silverlight\Bin\Release\%PKG%.Silverlight.dll lib\sl4
