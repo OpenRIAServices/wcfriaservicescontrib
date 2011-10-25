33c03676547d14d869558e1560012fa65545e726
MdeJ_cp <SND\MdeJ_cp>
2011-10-25 06:58:58 -0700

    EntityGraph: Added FullEntityGraphShape
    This is a dynamic graph shape that returns all paths from an entity for which
    the navigation properties are annotated with AssociationAttribute.

---
+++ EntityGraph/EntityGraph.Silverlight/RIA/FullEntityGraphShape.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace RiaServicesContrib.DomainServices.Client
+{
+    public class FullEntityGraphShape : IEntityGraphShape
+    {
+        /// <summary>
+        /// Returns an IEnumerable that iterates over the out edges of the given entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public IEnumerable<PropertyInfo> OutEdges(object entity)
+        {
+            return from propInfo in entity.GetType().GetProperties() 
+                   where propInfo.IsDefined(typeof(AssociationAttribute), true)
+                   select propInfo;
+        }
+        /// <summary>
+        /// Indicates of the given property info represents an edge in this graph shape object.
+        /// </summary>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        public bool IsEdge(PropertyInfo edge)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Returns the object that is reachable from entity via the given edge.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        public object GetNode(object entity, PropertyInfo edge)
+        {
+            return edge.GetValue(entity, null);
+        }
+        /// <summary>
+        /// Returns the collection of objects that is reachable from entity via the given edge.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="edge"></param>
+        /// <returns></returns>
+        public IEnumerable GetNodes(object entity, PropertyInfo edge)
+        {
+            var nodes = (IEnumerable)edge.GetValue(entity, null);
+            return nodes == null ? new List<object> { } : nodes;
+        }
+    }
+}
---
+++ EntityGraph/EntityGraphTests.Silverlight/EntityGraphTests/Tests/FullEntityGraphShapeTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiaServicesContrib;
+using RiaServicesContrib.DomainServices.Client;
+
+
+namespace EntityGraphTests.Tests
+{
+    [TestClass]
+    public class FullEntityGraphShapeTests : EntityGraphTest
+    {
+        public class A
+        {
+            [Association("ab", null, null)]
+            public B B { get; set; }
+
+            public int Foo { get; set; }
+        }
+        public class B
+        {
+            public int Bar { get; set; }
+            [Association("cs", null, null)]
+            public List<C> Cs { get; set; }
+            [Association("a", null, null)]
+            public A A { get; set; }
+        }
+        public class C
+        {
+            [Association("a", null, null)]
+            public A A { get; set; }
+        }
+        [TestMethod]
+        public void FullEntityGraphShapeTestSingleAssociations()
+        {
+            A a = new A { B = new B() };
+
+            var shape = new FullEntityGraphShape();
+
+            var outedges = shape.OutEdges(a);
+            Assert.IsTrue(outedges.Count() == 1);
+            Assert.IsTrue(outedges.Single() == typeof(A).GetProperty("B"));
+
+            var graph = new EntityGraph<object>(a, shape);
+            Assert.IsTrue(graph.Count() == 2);
+        }
+        [TestMethod]
+        public void FullEntityGraphShapeTestMultiAssociations()
+        {
+            B b = new B();
+            var shape = new FullEntityGraphShape();
+            var outedges = shape.OutEdges(b);
+            Assert.IsTrue(outedges.Count() == 2);
+        }
+        [TestMethod]
+        public void FullEntityGraphShapeTestCollectionAssociations()
+        {
+            A a = new A { B = new B() };
+            a.B.Cs = new List<C>() { new C() };
+
+            var shape = new FullEntityGraphShape();
+
+            var outedges = shape.OutEdges(a.B);
+            Assert.IsTrue(outedges.Count() == 2);
+
+            var collection = outedges.Single(x => x.Name == "Cs");
+
+            Assert.IsTrue(collection == typeof(B).GetProperty("Cs"));
+
+            var graph = new EntityGraph<object>(a, shape);
+            Assert.IsTrue(graph.Count() == 3);
+        }
+        [TestMethod]
+        public void FullEntityGraphShapeTestCycle()
+        {
+            A a = new A { B = new B() };
+            a.B.Cs = new List<C>() { new C { A = a } };
+
+            var shape = new FullEntityGraphShape();
+
+            var graph = new EntityGraph<object>(a, shape);
+            Assert.IsTrue(graph.Count() == 3);
+        }
+    }
+}
