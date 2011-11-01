2e4aef6d802370221f3f20c8cb722e75bc0f2757
MdeJ_cp <SND\MdeJ_cp>
2011-11-01 14:42:13 -0700

    EntityGraph: Refactored FullEntityGraphShape into a generic DynamicGraphShape
    DynamicGraphShape uses a property filter to match properties that form edges in the graph.
    This class has now become framework-independent and is therefore part of EntityGraph.Net
        
    FullEntityGraphShape uses the property filter prop => prop.IsDefined(typeof(AssociationAttribute), true)
    to select properties marked with the AssociationAttribute as edges.

--- EntityGraph/EntityGraph.Silverlight/RIA/FullEntityGraphShape.cs
+++ EntityGraph/EntityGraph.Silverlight/RIA/FullEntityGraphShape.cs
@@ -1,54 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RiaServicesContrib.DomainServices.Client
 {
-    public class FullEntityGraphShape : IEntityGraphShape
+    /// <summary>
+    /// This class defines an entity graph shape that spans all associations in your object
+    /// graph that are annotated with the AssociationAttribute.
+    /// </summary>
+    public class FullEntityGraphShape : DynamicGraphShape
     {
         /// <summary>
-        /// Returns an IEnumerable that iterates over the out edges of the given entity
+        /// Initializes a new instance of the FullEntityGraphShape class
         /// </summary>
-        /// <param name="entity"></param>
-        /// <returns></returns>
-        public IEnumerable<PropertyInfo> OutEdges(object entity)
+        public FullEntityGraphShape() :
+            base(prop => prop.IsDefined(typeof(AssociationAttribute), true))
         {
-            return from propInfo in entity.GetType().GetProperties() 
-                   where propInfo.IsDefined(typeof(AssociationAttribute), true)
-                   select propInfo;
-        }
-        /// <summary>
-        /// Indicates of the given property info represents an edge in this graph shape object.
-        /// </summary>
-        /// <param name="edge"></param>
-        /// <returns></returns>
-        public bool IsEdge(PropertyInfo edge)
-        {
-            throw new NotImplementedException();
-        }
-        /// <summary>
-        /// Returns the object that is reachable from entity via the given edge.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <param name="edge"></param>
-        /// <returns></returns>
-        public object GetNode(object entity, PropertyInfo edge)
-        {
-            return edge.GetValue(entity, null);
-        }
-        /// <summary>
-        /// Returns the collection of objects that is reachable from entity via the given edge.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <param name="edge"></param>
-        /// <returns></returns>
-        public IEnumerable GetNodes(object entity, PropertyInfo edge)
-        {
-            var nodes = (IEnumerable)edge.GetValue(entity, null);
-            return nodes == null ? new List<object> { } : nodes;
         }
     }
 }
