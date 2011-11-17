b1bb9c41099944395cbb29373ab3b3fa53d2cad4
MdeJ_cp <SND\MdeJ_cp>
2011-11-17 02:58:09 -0800

    EntityGraph: EntityGraph now implements System.ComponentModel.IChangeTracking

---
+++ EntityGraph/EntityGraph.Net/IChangeTracking.shared.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace RiaServicesContrib
+{
+    public partial class EntityGraph<TEntity> : IChangeTracking
+    {
+        /// <summary>
+        /// Resets the entity graph’s state to unchanged by accepting the modifications of all its entities.
+        /// </summary>
+        public void AcceptChanges()
+        {
+            foreach(var entity in this.OfType<IChangeTracking>())
+            {
+                entity.AcceptChanges();
+            }
+        }
+        /// <summary>
+        /// Gets the changed status of the entity graph.
+        /// </summary>
+        public bool IsChanged
+        {
+            get {
+                return this.OfType<IChangeTracking>().Aggregate(false, (isChanged, e) => isChanged | e.IsChanged);            
+            }
+        }
+    }
+}
---
+++ EntityGraph/EntityGraphTests.Silverlight/EntityGraphTests/Tests/IChangeTrackingTests.cs
@@ -0,0 +1,25 @@
+﻿using EntityGraphTests.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiaServicesContrib.DomainServices.Client;
+
+namespace EntityGraphTests.Tests
+{
+    [TestClass]
+    public class IChangeTrackingTests : EntityGraphTest
+    {
+        [TestMethod]
+        public void IsChangedAndAcceptChangesTest()
+        {
+            EntityGraphTestsDomainContext ctx = new EntityGraphTestsDomainContext();
+            ctx.As.Attach(a);
+            var graph = new EntityGraph(a, new FullEntityGraphShape());
+            Assert.IsFalse(graph.IsChanged);
+
+            a.B.name = "Changed";
+            Assert.IsTrue(graph.IsChanged);
+
+            graph.AcceptChanges();
+            Assert.IsFalse(graph.IsChanged);
+        }
+    }
+}
